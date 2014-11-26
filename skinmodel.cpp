8ff1a397b9af2941d3326c20037d87ed63b4e78d
Sebastian Bittel <sebastian.bittel@gmail.com>
2014-11-26 10:38:05 +0100

    gauss added

--- skinmodel.cpp
+++ skinmodel.cpp
@@ -21,6 +21,13 @@ double prior_skin;
 double prior_no_skin;
 int hist_counter;
 
+
+
+
+cv::Mat skin_Covar, skin_Mu;
+Mat skin_gauss;
+Mat no_skin_gauss;
+
 /// Constructor
 SkinModel::SkinModel()
 {
@@ -55,13 +62,46 @@ void SkinModel::startTraining()
 void SkinModel::train(const cv::Mat3b& img, const cv::Mat1b& mask)
 {
     
+    int blur_size = 3;
     //get HSV
     Mat hsv;
     cvtColor(img, hsv, CV_BGR2HSV);
-    //get Normalized RGB (aka rgb)
     
+    //smooth image
+    GaussianBlur(hsv, hsv, Size(blur_size, blur_size), 0, 0);
     
-    //imshow("no ssdfsfkin", hsv);
+    //Seb with gauss
+    for (int row = 0; row < hsv.rows; ++row) {
+        for (int col = 0; col < hsv.cols; ++col) {
+            
+            cv::Vec3b hsv_ = hsv.at<cv::Vec3b>(row, col);
+
+            
+            double h__ = (double)hsv_[0];
+            double s__ = (double)hsv_[1];
+           
+            double temp [] = {h__, s__};
+            Mat hs_row = Mat(1,2,  CV_64F,temp);
+            
+            if(mask.at<int>(row, col) >0){
+                
+                skin_gauss.push_back(hs_row);
+            
+            }else{
+                no_skin_gauss.push_back(hs_row);
+            }
+            
+            
+            
+        }
+    
+    }
+    /*
+    vector<Mat> hsv_planes;
+    split( hsv, hsv_planes );
+    hsv_planes[0].mul(mask/255);
+    */
+    
     /// Using 50 bins for hue and 60 for saturation
     int h_bins = 50; int s_bins = 60;
     int histSize[] = { h_bins, s_bins };
@@ -80,18 +120,6 @@ void SkinModel::train(const cv::Mat3b& img, const cv::Mat1b& mask)
     
     calcHist( &hsv, 1, channels, ~mask, hist_no_skin, 2, histSize, ranges, true, true );
     
-    
-    //imshow("asd", temp_hist_skin);
-    //imshow("no skin", temp_hist_no_skin);
-    
-    //cout << temp_hist_skin <<endl;
-    //waitKey(0);
-    
-    // normalize( hist_base, hist_base, 0, 1, NORM_MINMAX, -1, Mat() );
-    
-    //--- IMPLEMENT THIS ---//
-    
-    //train with mixture of gaussians
     hist_counter++;
 }
 
@@ -103,8 +131,18 @@ void SkinModel::train(const cv::Mat3b& img, const cv::Mat1b& mask)
 void SkinModel::finishTraining()
 {
     
-    //cout << hist_skin.size() << endl;
-    //cout << hist_skin << endl;
+    //calcualte cov and mean of skin
+    cv::calcCovarMatrix(skin_gauss,skin_Covar, skin_Mu, CV_COVAR_NORMAL | CV_COVAR_ROWS |CV_COVAR_SCALE );
+
+    
+    
+    //calcualte cov and mean of no _skin
+    cv::Mat Covar_, Mu_;
+    cv::calcCovarMatrix(no_skin_gauss,Covar_, Mu_, CV_COVAR_NORMAL | CV_COVAR_ROWS |CV_COVAR_SCALE );
+   // waitKey(0);
+
+    
+
     double skin_hist_sum = cv::sum(hist_skin)[0];
     double no_skin_hist_sum = cv::sum(hist_no_skin)[0];
     prior_skin = skin_hist_sum / (skin_hist_sum + no_skin_hist_sum);
@@ -115,26 +153,8 @@ void SkinModel::finishTraining()
     normalize(hist_no_skin,  hist_no_skin, 1, 1, NORM_L1, -1, Mat());
     
     decision_rule = (hist_skin > hist_no_skin);
-    //cout << "Normalized: " << hist_skin.size() << endl;
-    //cout << hist_skin << endl;
-    
-    
-    //cout << temp_hist_skin <<endl;
-    //waitKey(0);
-    
-    //cvNormalizeHist(hist_skin,1);
-    
-    /* for (int ubin=0; ubin < hist_bins[0]; ubin++) {
-     for (int vbin = 0; vbin < hist_bins[1]; vbin++) {
-     if (skin_hist.at<float>(ubin,vbin) > 0) {
-     skin_hist.at<float>(ubin,vbin) /= skin_pixels;
-     }
-     if (non_skin_hist.at<float>(ubin,vbin) > 0) {
-     non_skin_hist.at<float>(ubin,vbin) /= non_skin_pixels;
-     }
-     }
-     */
-    //--- IMPLEMENT THIS ---//
+
+ 
 }
 
 
@@ -153,16 +173,52 @@ cv::Mat1b SkinModel::classify(const cv::Mat3b& img)
     for (int row = 0; row < img.rows; ++row) {
         for (int col = 0; col < img.cols; ++col) {
             //Martina
-            if (true) {
+            if (false) {
                 cv::Vec3b pixel = hsv.at<cv::Vec3b>(row, col);
                 float h_bin_size = 180.0/50.0;
                 float s_bin_size = 256.0/60.0;
-                int h = pixel[0]/h_bin_size;
-                int s = pixel[1]/s_bin_size;
+                int h = ((int)pixel[0])/h_bin_size;
+                int s = ((int)pixel[1])/s_bin_size;
+               // cout << "prior no skin"<< << endl;
                 if (((hist_skin.at<int>(s, h)*prior_skin) / prior_no_skin) > ((hist_no_skin.at<int>(s, h)*prior_no_skin) / prior_skin)) {
                     skin(row, col) = 255;
                 }
             }
+            
+            //Seb gauss
+            if (true) {
+                //for skin use formular slide 31
+                cv::Vec3b pixel = hsv.at<cv::Vec3b>(row, col);
+                double h = pixel[0];
+                double s = pixel[1];
+                double temp [] = {h, s};
+                Mat x = Mat(1,2,  CV_64F,temp);
+     
+               
+                Mat x_mu = x - skin_Mu;
+                Mat x_mu_t = x_mu.t();
+                
+                
+                double det_cov = determinant(skin_Covar);
+                Mat cov_inv = skin_Covar.inv();
+                
+                double first_ = 0.5 * sqrt(det_cov);
+                Mat exp_ = -0.5 * x_mu * cov_inv * x_mu_t;
+                double exponent = exp_.at<double>(0,0);
+
+                
+                
+                double probab = first_ * exp(exponent);
+   
+               // waitKey(0);
+               
+                if (probab > 0) {
+                    skin(row, col) = 1;
+                }
+            }
+            
+            
+            
             if (false)
                 skin(row, col) = rand()%256;
             
@@ -185,8 +241,8 @@ cv::Mat1b SkinModel::classify(const cv::Mat3b& img)
     //erosion and dilation to reduce mistakes
     //first morphological closing (delatation -> erosion) and then opening (erosion -> delatation)
     //MORPH_CLOSE 3     MORPH_OPEN 2
-    //cv::morphologyEx( skin, skin, MORPH_CLOSE, element);
-    //cv::morphologyEx( skin, skin, MORPH_OPEN, element);
+    cv::morphologyEx( skin, skin, MORPH_CLOSE, element);
+    cv::morphologyEx( skin, skin, MORPH_OPEN, element);
     
     
     
